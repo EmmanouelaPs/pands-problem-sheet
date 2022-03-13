53ecb320271f56bb99dec765a4b245c075b162e8
Emma Psaraki <G00411374@gmit.ie>
2022-03-13 01:27:37 +0000

    Initial commit

---
+++ bmi.py
@@ -0,0 +1,7 @@
+weightkg =int(input('enter weight(kg)'))
+heightcm = int(input('enter height(cm)'))
+heightm2 = (heightcm / 100)
+print (heightm2)
+BMI = weightkg/(heightm2**2)
+print (BMI)
+print ('your BMI is {}'.format(BMI))
